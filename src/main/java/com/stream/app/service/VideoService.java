05dc43d25b33877c53df994df62d840b51f9eb50
Euphoria99 <363pavanbhat@gmail.com>
2024-09-23 23:32:48 +0100

    Video Service

---
+++ src/main/java/com/stream/app/service/VideoService.java
@@ -0,0 +1,24 @@
+package com.stream.app.service;
+
+import com.stream.app.entities.Video;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+
+public interface VideoService {
+
+    //save
+    Video save(String video, MultipartFile file);
+
+
+    //get video by id
+    Video get(String videoId);
+
+    //get video by title
+    Video getByTitle(String title);
+
+    //get all video
+    List<Video> getAll();
+}
