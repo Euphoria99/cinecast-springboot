48ff561722e0692a60b047453e9bacf0cfc164c5
Euphoria99 <363pavanbhat@gmail.com>
2024-10-01 15:30:00 +0100

    Major: Updated Stream API to send chunks

--- src/main/java/com/stream/app/controllers/VideoController.java
+++ src/main/java/com/stream/app/controllers/VideoController.java
@@ -1,8 +1,10 @@
 package com.stream.app.controllers;
 
+import com.stream.app.AppConstants;
 import com.stream.app.entities.Video;
 import com.stream.app.payload.ErrorMessage;
 import com.stream.app.service.VideoService;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -129,17 +131,24 @@ public class VideoController {
         System.out.println("Ranges after --> " + Arrays.toString(ranges));
         rangeStart = Long.parseLong(ranges[0]);
 
-        // Check if the range includes an end
-        if (ranges.length > 1 && !ranges[1].isEmpty()) {
-            rangeEnd = Long.parseLong(ranges[1]);
-        } else {
-            rangeEnd = fileLength - 1; // If no end specified, go until the end of the file
+        rangeEnd = rangeStart + AppConstants.CHUNK_SIZE -1;
+
+        if(rangeEnd >= fileLength){
+            rangeEnd = fileLength -1;
         }
 
-        // Adjust if rangeEnd exceeds fileLength
-        if (rangeEnd > fileLength - 1) {
-            rangeEnd = fileLength - 1;
-        }
+
+        // Check if the range includes an end
+//        if (ranges.length > 1 && !ranges[1].isEmpty()) {
+//            rangeEnd = Long.parseLong(ranges[1]);
+//        } else {
+//            rangeEnd = fileLength - 1; // If no end specified, go until the end of the file
+//        }
+//
+//        // Adjust if rangeEnd exceeds fileLength
+//        if (rangeEnd > fileLength - 1) {
+//            rangeEnd = fileLength - 1;
+//        }
 
         System.out.println("Range Start: " + rangeStart);
         System.out.println("Range End: " + rangeEnd);
@@ -148,25 +157,32 @@ public class VideoController {
         try {
             inputStream = Files.newInputStream(path);
             inputStream.skip(rangeStart);
+            long contentLength = rangeEnd - rangeStart + 1;
+
+            byte[] data = new byte[(int) contentLength];
+            int read = inputStream.read(data,0, data.length);
+            System.out.println("read(number of bytes) : " +read);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.add("X-Content-Type-Options", "nosniff");
+
+            headers.setContentLength(contentLength);
+
+            return ResponseEntity
+                    .status(HttpStatus.PARTIAL_CONTENT)
+                    .headers(headers)
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(new ByteArrayResource(data));
+
         } catch (IOException ex) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
-        long contentLength = rangeEnd - rangeStart + 1;
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
-        headers.add("X-Content-Type-Options", "nosniff");
 
-        headers.setContentLength(contentLength);
-
-        return ResponseEntity
-                .status(HttpStatus.PARTIAL_CONTENT)
-                .headers(headers)
-                .contentType(MediaType.parseMediaType(contentType))
-                .body(new InputStreamResource(inputStream));
     }
 
 }
