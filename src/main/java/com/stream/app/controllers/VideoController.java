165fe8976697ea3e151d60cf1c71e2e237903763
Euphoria99 <363pavanbhat@gmail.com>
2024-10-04 18:30:00 +0100

    serving hls file: Master file

--- src/main/java/com/stream/app/controllers/VideoController.java
+++ src/main/java/com/stream/app/controllers/VideoController.java
@@ -186,4 +186,27 @@ public class VideoController {
 
     }
 
+    @Value("${files.video.hls}")
+    private String HLS_DIR;
+
+    @GetMapping("/{videoId}/master.m3u8")
+    public ResponseEntity<Resource> streamMaster(
+            @PathVariable String videoId
+    ){
+        Path path = Paths.get(HLS_DIR,videoId,"master.m3u8");
+
+        System.out.println("The path->" + path);
+
+        if(!Files.exists(path)){
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Resource resource = new FileSystemResource(path);
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_TYPE, "application/vnd.apple.mpegurl")
+                .body(resource);
+    }
+
 }
